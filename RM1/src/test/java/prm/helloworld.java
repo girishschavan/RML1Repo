a1cd1cb15fa95d303e845213dae44cfcd4c70867
Girish Chavan <girishschavan@gmail.com>
2016-07-12 21:43:46 -0700

    intial commit

---
+++ RM1/src/test/java/prm/helloworld.java
@@ -0,0 +1,42 @@
+package prm;
+
+import java.util.concurrent.TimeUnit;
+
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class helloworld {
+	WebDriver driver;
+	
+	@BeforeTest
+	public void setup()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Girish\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-extensions");
+		driver = new ChromeDriver(options);
+		driver.get("http://www.google.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+		
+	
+	@Test
+	public void testAllLinks() throws InterruptedException
+	{
+
+		
+	System.out.println(driver.getCurrentUrl());
+		
+	}
+}
+
+	
+	
+
