8ef47d86f901df9c0a525a34a3bace2372c936a8
Girish Chavan <girishschavan@gmail.com>
2016-07-17 15:40:14 -0700

    test

--- RM1/src/test/java/prm/helloworld.java
+++ RM1/src/test/java/prm/helloworld.java
@@ -14,7 +14,7 @@ import org.testng.annotations.Test;
 public class helloworld {
 	WebDriver driver;
 	
-	@BeforeTest
+	/*@BeforeTest
 	public void setup()
 	{
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Girish\\Downloads\\chromedriver_win32\\chromedriver.exe");
@@ -24,13 +24,20 @@ public class helloworld {
 		driver.get("http://www.google.com");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	}
+	}*/
 		
 	
 	@Test
 	public void testAllLinks() throws InterruptedException
 	{
-
+		System.out.println("inside test method");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Girish\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-extensions");
+		driver = new ChromeDriver(options);
+		driver.get("http://www.google.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
 	System.out.println(driver.getCurrentUrl());
 		
