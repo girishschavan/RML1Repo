8ffdc6fbf9606a9ef1c42c84328bb68a7ba6ed29
Girish Chavan <girishschavan@gmail.com>
2016-07-17 17:24:54 -0700

    display

--- RM1/src/test/java/prm/helloworld.java
+++ RM1/src/test/java/prm/helloworld.java
@@ -1,19 +1,24 @@
 package prm;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.google.common.collect.ImmutableMap;
+
 public class helloworld {
 	WebDriver driver;
-	
+//	HtmlUnitDriver driver;
 	/*@BeforeTest
 	public void setup()
 	{
@@ -28,19 +33,29 @@ public class helloworld {
 		
 	
 	@Test
-	public void testAllLinks() throws InterruptedException
+	public void testAllLinks() throws InterruptedException, IOException
 	{
 		System.out.println("inside test method");
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Girish\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		
+		ChromeDriverService service = new ChromeDriverService.Builder()
+				  .usingDriverExecutable(new File("C:\\Users\\Girish\\Downloads\\chromedriver_win32\\chromedriver.exe"))
+				  .usingAnyFreePort()
+				  .withEnvironment(ImmutableMap.of("DISPLAY",":10"))
+				  .build();
+				  service.start();
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Girish\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--disable-extensions");
 		driver = new ChromeDriver(options);
+		//driver = new HtmlUnitDriver();
 		driver.get("http://www.google.com");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
 	System.out.println(driver.getCurrentUrl());
-		
+	
+	driver.quit();
+	service.stop();
 	}
 }
 
